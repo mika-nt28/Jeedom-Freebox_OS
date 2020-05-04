58cef268a3faf0a99098ac7f7165940a726b588c
Halbout Michael <michaelhalbout@gmail.com>
2020-05-04 17:41:12 +0200

    Update Freebox_OS.php

--- desktop/php/Freebox_OS.php
+++ desktop/php/Freebox_OS.php
@@ -31,12 +31,68 @@
 		<div class="eqLogicThumbnailContainer">
     		<?php
 			foreach ($eqLogics as $eqLogic) {
-				$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
-				echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
-				echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
-				echo '<br>';
-				echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
-				echo '</div>';
+				switch ($eqLogic->getLogicalId()){
+					case 'AirPlay':
+					case 'ADSL':
+					case 'Downloads':
+					case 'System':
+					case 'Disque':						
+					case 'Phone':
+					case'Reseau':
+						$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+						echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
+						echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+						echo '<br>';
+						echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+						echo '</div>';
+					break;		
+				}
+			}
+		?>
+		</div>
+		
+  		<legend><i class="fas fa-table"></i> {{Home}}</legend>
+	   	<input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
+		<div class="eqLogicThumbnailContainer">
+    		<?php
+			foreach ($eqLogics as $eqLogic) {
+				switch ($eqLogic->getLogicalId()){
+					case'HomeAdapters':
+						$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+						echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
+						echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+						echo '<br>';
+						echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+						echo '</div>';
+					break;
+				}
+			}
+		?>
+		</div>
+  		<legend><i class="fas fa-table"></i> {{Tiles}}</legend>
+	   	<input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
+		<div class="eqLogicThumbnailContainer">
+    		<?php
+			foreach ($eqLogics as $eqLogic) {
+				switch ($eqLogic->getLogicalId()){
+					case 'AirPlay':
+					case 'ADSL':
+					case 'Downloads':
+					case 'System':
+					case 'Disque':						
+					case 'Phone':
+					case'Reseau':
+					case'HomeAdapters':
+					break;
+					default:
+						$opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+						echo '<div class="eqLogicDisplayCard cursor '.$opacity.'" data-eqLogic_id="' . $eqLogic->getId() . '">';
+						echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+						echo '<br>';
+						echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+						echo '</div>';
+					break;
+				}
 			}
 		?>
 		</div>

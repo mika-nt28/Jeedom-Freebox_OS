a240b624b4be69c1f4c031347f3d03ee8f8a9112
JAG <48254791+JEALG@users.noreply.github.com>
2020-05-31 15:04:23 +0200

    change widget

--- core/class/Freebox_OS.class.php
+++ core/class/Freebox_OS.class.php
@@ -305,12 +305,13 @@ class Freebox_OS extends eqLogic {
             $Commande->setSubType($SubType);
             $Commande->setDisplay('generic_type',$generic_type);
             if ($Template !='default') {
-                $Commande->setTemplate('dashboard','Freebox_OS::'.$Template);
+		    $Commande->setTemplate('dashboard','Freebox_OS::'.$Template);
+		    $Commande->setTemplate('mobile', 'Freebox_OS::'.$Template);
             } else {
                 $Commande->setTemplate('dashboard','default');
                 $Commande->setTemplate('mobile','default');
             }
-            $Commande->setTemplate('mobile', 'Freebox_OS::'.$Template);
+            
             $Commande->save();
         }
         $refresh = $this->getCmd(null, 'refresh');

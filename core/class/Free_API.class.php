cf78d8052e3e95e5c5fdf6fa757ba555653203ee
JEALG <jalain.gosse@icloud.com>
2020-07-28 10:12:51 +0200

    Amélioration

--- core/class/Free_API.class.php
+++ core/class/Free_API.class.php
@@ -245,7 +245,7 @@ class Free_API
 				$logicalinfo = Freebox_OS::getlogicalinfo();
 				$disk = Freebox_OS::AddEqLogic($logicalinfo['diskName'], $logicalinfo['diskID'], 'default', false, null, null);
 
-				$command = $disk->AddCommand('Disque - ' . $disks['type']  . ' - (Id ' . $disks['id'] . ')', $disks['id'], 'info', 'numeric', 'Freebox_OS::Freebox_OS_Disque', '%', null, 1, 'default', 'default', 0, 'fas fa-save', 0, '0', 100,  null, '0', false, false, 'never', null, true);
+				$command = $disk->AddCommand('Occupation du disque - ' . $disks['type']  . ' - (Id ' . $disks['id'] . ')', $disks['id'], 'info', 'numeric', 'Freebox_OS::Freebox_OS_Disque', '%', null, 1, 'default', 'default', 0, 'fas fa-save', 0, '0', 100,  null, '0', false, false, 'never', null, true);
 				$command->event($value);
 				log::add('Freebox_OS', 'debug', '└─────────');
 			}

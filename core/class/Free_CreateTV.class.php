2c285390dbae6036a55a7c75dd148b8a1bea2981
JEALG <jalain.gosse@icloud.com>
2020-08-10 18:30:44 +0200

    Désactivation fonction en test

--- core/class/Free_CreateTV.class.php
+++ core/class/Free_CreateTV.class.php
@@ -68,10 +68,10 @@ class Free_CreateTV
             $player->AddCommand('API Disponible', 'api_available', 'info', 'binary', null, null, null, 0, 'default', 'default', 0, null, 0, 'default', 'default', 5, '0', false, false);
             $player->AddCommand('Disponible sur le réseau', 'reachable', 'info', 'binary', null, null, null, 0, 'default', 'default', 0, null, 0, 'default', 'default', 6, '0', false, false);
             $player->AddCommand('Etat', 'power_state', 'info', 'string', $TemplatePlayer, null, null, 1, 'default', 'default', 0, null, 0, 'default', 'default', 7, '0', false, false);
-            $player->AddCommand('Play Pause', 'play_pause', 'action', 'message', null, null, null, 1, 'default', 'default', 0, $iconePlayOn, 0, 'default', 'default', 8, '0', false, false);
-            $player->AddCommand('Stop', 'stop', 'action', 'message', null, null, null, 1, 'default', 'default', 0, $iconePlayOff, 0, 'default', 'default', 9, '0', false, false);
-            $player->AddCommand('Précedent', 'previous', 'action', 'message', null, null, null, 1, 'default', 'default', 0, $iconePlayPrevious, 0, 'default', 'default', 10, '0', false, false);
-            $player->AddCommand('Suivant', 'next', 'action', 'message', null, null, null, 1, 'default', 'default', 0, $iconePlayNext, 0, 'default', 'default', 11, '0', false, false);
+            //$player->AddCommand('Play Pause', 'play_pause', 'action', 'message', null, null, null, 1, 'default', 'default', 0, $iconePlayOn, 0, 'default', 'default', 8, '0', false, false);
+            //$player->AddCommand('Stop', 'stop', 'action', 'message', null, null, null, 1, 'default', 'default', 0, $iconePlayOff, 0, 'default', 'default', 9, '0', false, false);
+            //$player->AddCommand('Précedent', 'previous', 'action', 'message', null, null, null, 1, 'default', 'default', 0, $iconePlayPrevious, 0, 'default', 'default', 10, '0', false, false);
+            //$player->AddCommand('Suivant', 'next', 'action', 'message', null, null, null, 1, 'default', 'default', 0, $iconePlayNext, 0, 'default', 'default', 11, '0', false, false);
 
             Free_Refresh::RefreshInformation($player->getId());
         }

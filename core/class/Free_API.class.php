74b933a2cc3a4ec10f155eb6ce9f43def2a016e8
JEALG <jalain.gosse@icloud.com>
2020-09-09 18:21:55 +0200

    Amélioration des logs

--- core/class/Free_API.class.php
+++ core/class/Free_API.class.php
@@ -177,9 +177,10 @@ class Free_API
                     log::add('Freebox_OS', 'error', 'Erreur Droits : ' . $result['msg']);
                     return false;
                 } else if ($result['error_code'] == "auth_required") {
-                    //$this->close_session();
-                    $this->getFreeboxOpenSessionData();
                     log::add('Freebox_OS', 'Debug', '[Redémarrage session à cause de l\'erreur] : ' . $result['msg']);
+                    $this->close_session();
+                    $this->getFreeboxOpenSessionData();
+                    log::add('Freebox_OS', 'Debug', '[Redémarrage session Terminée à cause de l\'erreur] : ' . $result['msg']);
                     return false;
                 } else if ($result['error_code'] == 'denied_from_external_ip') {
                     log::add('Freebox_OS', 'error', 'Erreur Accès : ' . $result['msg']);

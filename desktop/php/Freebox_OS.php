826ab94d01dd603450ebe97919f1c03dd3bc6e31
JEALG <jalain.gosse@icloud.com>
2020-11-28 18:34:24 +0100

    Modification BP + Info Bulle

--- desktop/php/Freebox_OS.php
+++ desktop/php/Freebox_OS.php
@@ -202,10 +202,10 @@ $eqLogics = eqLogic::byType($plugin->getId());
 		<div class="input-group pull-right" style="display:inline-flex;">
 			<span class="input-group-btn">
 				<!-- Les balises <a></a> sont volontairement fermées à la ligne suivante pour éviter les espaces. Ne pas modifier -->
-				<a class="btn btn-sm btn-default eqLogicAction roundedLeft" data-action="configure"><i class="fa fa-cogs"></i><span class="hidden-xs"> {{Configuration avancée}}</span>
-				</a><a class="btn btn-sm btn-info eqLogicAction Equipement"><i class="fas fa-search"></i><span class="hidden-xs"> {{Recherche des équipements supplémentaires}}</span>
+				<a class="btn btn-sm btn-default eqLogicAction roundedLeft" data-action="configure" title=" {{Configuration de l'équipement}}"><i class="fa fa-cogs"></i><span class="hidden-xs"> {{Configuration avancée}}</span>
+				</a><a class="btn btn-sm btn-info eqLogicAction Equipement"><i class="fas fa-search" title=" {{Recherche des équipements supplémentaires}}"></i><span class="hidden-xs"> {{Recherche des commandes}}</span>
 				</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}
-				</a><a class="btn btn-sm btn-danger eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}
+				</a><a class="btn btn-sm btn-danger eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i><span class="hidden-xs"> {{Supprimer}}</span>
 				</a>
 			</span>
 		</div>

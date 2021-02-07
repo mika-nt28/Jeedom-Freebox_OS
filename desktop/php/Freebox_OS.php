3f758bd03f0ecc9c20c9d82bf19c3ca558fd26bd
JEALG <jalain.gosse@icloud.com>
2021-02-07 08:51:48 +0100

    Icône type tiles + mode tableau v4.2

--- desktop/php/Freebox_OS.php
+++ desktop/php/Freebox_OS.php
@@ -60,10 +60,11 @@ $eqLogics = eqLogic::byType($plugin->getId());
 			</div>
 		</div>
 		<!-- Champ de recherche -->
-		<div class="input-group" style="margin:5px;">
+		<div class="input-group" style="margin-bottom:5px;">
 			<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
 			<div class="input-group-btn">
-				<a id="bt_resetSearch" class="btn roundedRight" style="width:30px"><i class="fas fa-times"></i> </a>
+				<a id="bt_resetObjectSearch" class="btn" style="width:30px"><i class="fas fa-times"></i>
+				</a><a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>
 			</div>
 		</div>
 		<!-- Liste des équipements du plugin "Mes équipements" -->
@@ -117,8 +118,12 @@ $eqLogics = eqLogic::byType($plugin->getId());
 				<?php
 				$status = 0;
 				foreach ($eqLogics as $eqLogic) {
-					if ($eqLogic->getConfiguration('type') == 'parental' || $eqLogic->getConfiguration('type') == 'player' || $eqLogic->getConfiguration('type') == 'alarm_control' || $eqLogic->getConfiguration('type') == 'alarm_sensor' || $eqLogic->getConfiguration('type') == 'alarm_remote') {
+					if ($eqLogic->getConfiguration('type') == 'parental' || $eqLogic->getConfiguration('type') == 'player' || $eqLogic->getConfiguration('type') == 'alarm_control' || $eqLogic->getConfiguration('type') == 'alarm_sensor' || $eqLogic->getConfiguration('type') == 'camera' || $eqLogic->getConfiguration('type') == 'alarm_remote') {
+						//if ($eqLogic->getConfiguration('type') === 'alarm_sensor' && $eqLogic->getConfiguration('type2') === 'dws') {
+						//	$template = $eqLogic->getConfiguration('type2');
+						//} else {
 						$template = $eqLogic->getConfiguration('type');
+						//}
 						$icon = $template;
 					} else {
 						$template = $eqLogic->getLogicalId();

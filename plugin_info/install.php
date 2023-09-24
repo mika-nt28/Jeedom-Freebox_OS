7ea5bf3156e5779a191e162457ea949ed5ee3a11
JEALG <jalain.gosse@icloud.com>
2023-09-24 10:38:03 +0200

    Correctifs + Communauty
    - Gestion de la télécommande de l'alarme de la Freebox
    - Amélioration info vers communauty
    - Ajout date du lancement des scan

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -156,7 +156,7 @@ function Freebox_OS_update()
 			config::save('FREEBOX_REBOOT_DEAMON', FALSE, 'Freebox_OS');
 		}
 
-		//message::add('Freebox_OS', 'Merci pour la mise à jour de ce plugin, n\'oubliez pas de lancer les divers Scans afin de bénéficier des nouveautés');
+		message::add('Freebox_OS', 'Cette mise nécessite de lancer les divers Scans afin de bénéficier des nouveautés et surtout des correctifs');
 	} catch (Exception $e) {
 		$e = print_r($e, 1);
 		log::add('Freebox_OS', 'error', 'Freebox_OS update ERROR : ' . $e);
